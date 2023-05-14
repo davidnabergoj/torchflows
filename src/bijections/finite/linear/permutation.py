970aac6ab637fed7970c892591a8779f64563386
David Nabergoj <dn9134@student.uni-lj.si>
2023-05-15 01:38:18 +0200

    Add permutation bijection

--- src/bijections/finite/linear/permutation.py
+++ src/bijections/finite/linear/permutation.py
@@ -5,11 +5,18 @@ from src.bijections.finite.base import Bijection
 
 
 class Permutation(Bijection):
-    def __init__(self):
+    def __init__(self, n_dim: int):
         super().__init__()
+        self.forward_permutation = torch.randperm(n_dim)
+        self.inverse_permutation = torch.empty_like(self.forward_permutation)
+        self.inverse_permutation[self.forward_permutation] = torch.arange(n_dim)
 
     def forward(self, x) -> Tuple[torch.Tensor, torch.Tensor]:
-        pass
+        z = x[:, self.forward_permutation]
+        log_det = torch.zeros(x.shape[0])
+        return z, log_det
 
     def inverse(self, z) -> Tuple[torch.Tensor, torch.Tensor]:
-        pass
+        x = z[:, self.inverse_permutation]
+        log_det = torch.zeros(z.shape[0])
+        return x, log_det
