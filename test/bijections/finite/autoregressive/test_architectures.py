cd5fee1142dac993b72c29820ab0259e56d5fba2
David Nabergoj <davidnabergoj4@gmail.com>
2023-05-19 01:24:56 +0200

    Separate architecture test for RealNVP and MAF

--- test/bijections/finite/autoregressive/test_architectures.py
+++ test/bijections/finite/autoregressive/test_architectures.py
@@ -5,15 +5,13 @@ from src.bijections.finite.autoregressive.architectures import RealNVP, MAF
 
 
 @pytest.mark.parametrize('n_dim', [2, 10, 100])
-@pytest.mark.parametrize('architecture_class', [RealNVP, MAF])
-def test_architecture(n_dim, architecture_class):
+def test_maf(n_dim):
+    # MAF reconstruction errors are larger with fewer input dimensions
     torch.manual_seed(0)
-    bijection = architecture_class(n_dim)
+    bijection = MAF(n_dim)
 
-    x = torch.randn(size=(5, n_dim))
-    print()
+    x = torch.randn(size=(125, n_dim)) * 5
     z, log_det_forward = bijection(x)
-    print()
 
     assert x.shape == z.shape
     assert log_det_forward.shape == (x.shape[0],)
@@ -23,5 +21,69 @@ def test_architecture(n_dim, architecture_class):
     assert x_reconstructed.shape == x.shape
     assert log_det_inverse.shape == (x.shape[0],)
 
-    assert torch.allclose(x, x_reconstructed, atol=1e-1)
-    assert torch.allclose(log_det_forward, -log_det_inverse, atol=1e-2)
+    if not torch.isclose(
+            reconstruction_error := (x - x_reconstructed).abs().max(),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(reconstruction_error) = }')
+
+    if not torch.isclose(
+            reconstruction_error := torch.linalg.norm(x - x_reconstructed),
+            torch.zeros(1),
+            atol=1e-3):
+        print(f'{reconstruction_error = }')
+        raise ValueError(f'{float(reconstruction_error) = }')
+
+    if not torch.isclose(
+            log_det_error := (log_det_forward + log_det_inverse).abs().max(),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(log_det_error) = }')
+
+    if not torch.isclose(
+            log_det_error := torch.linalg.norm(log_det_forward + log_det_inverse),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(log_det_error) = }')
+
+
+@pytest.mark.parametrize('n_dim', [2, 10, 100])
+def test_real_nvp(n_dim):
+    torch.manual_seed(0)
+    bijection = RealNVP(n_dim)
+
+    x = torch.randn(size=(125, n_dim)) * 5
+    z, log_det_forward = bijection(x)
+
+    assert x.shape == z.shape
+    assert log_det_forward.shape == (x.shape[0],)
+
+    x_reconstructed, log_det_inverse = bijection.inverse(z)
+
+    assert x_reconstructed.shape == x.shape
+    assert log_det_inverse.shape == (x.shape[0],)
+
+    if not torch.isclose(
+            reconstruction_error := (x - x_reconstructed).abs().max(),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(reconstruction_error) = }')
+
+    if not torch.isclose(
+            reconstruction_error := torch.linalg.norm(x - x_reconstructed),
+            torch.zeros(1),
+            atol=1e-3):
+        print(f'{reconstruction_error = }')
+        raise ValueError(f'{float(reconstruction_error) = }')
+
+    if not torch.isclose(
+            log_det_error := (log_det_forward + log_det_inverse).abs().max(),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(log_det_error) = }')
+
+    if not torch.isclose(
+            log_det_error := torch.linalg.norm(log_det_forward + log_det_inverse),
+            torch.zeros(1),
+            atol=1e-3):
+        raise ValueError(f'{float(log_det_error) = }')
