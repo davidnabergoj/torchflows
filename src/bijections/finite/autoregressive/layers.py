Fix IAF, test backprop

src/bijections/finite/autoregressive/layers.py
@@ -248,11 +248,11 @@ class ForwardMaskedAutoregressive(Bijection):
 
     def inverse(self, z) -> Tuple[torch.Tensor, torch.Tensor]:
         log_det = torch.zeros(size=(z.shape[0],), device=z.device)
+        x = torch.clone(z)
         for i in torch.arange(z.shape[-1]):  # FIXME this probably messes up autograd b/c it overwrites the gradient?
-            h = self.conditioner(z)
-            tmp, log_det = self.transformer.inverse(z, h)
-            z[:, i] = tmp[:, i]
-        x = z
+            h = self.conditioner(torch.clone(x))
+            tmp, log_det = self.transformer.inverse(x, h)
+            x[:, i] = tmp[:, i]
         return x, log_det
 
 

src/flows.py
@@ -8,7 +8,7 @@ class Flow(nn.Module):
     def __init__(self,
                  n_dim: int,
                  bijection: Bijection,
-                 device: torch.device):
+                 device: torch.device = torch.device('cpu')):
         super().__init__()
         self.base = torch.distributions.MultivariateNormal(
             loc=torch.zeros(n_dim, device=device),