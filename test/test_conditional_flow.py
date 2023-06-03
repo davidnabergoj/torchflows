87b2793c52b182b5186da7d2812bf895aa0c0e06
David Nabergoj <davidnabergoj4@gmail.com>
2023-06-03 18:37:57 +0200

    Add CUDA as a test for conditional flows

--- test/test_conditional_flow.py
+++ test/test_conditional_flow.py
@@ -6,18 +6,20 @@ from src import RealNVP, Flow
 
 @pytest.mark.parametrize('n_dim', [2, 10, 100])
 @pytest.mark.parametrize('context_dim', [1, 2, 10, 100])
-def test_context(n_dim, context_dim):
+@pytest.mark.parametrize('device', ['cpu', 'cuda'])
+def test_context(n_dim, context_dim, device: str):
     torch.manual_seed(0)
+    device = torch.device(device)
 
     n_data = 100
-    flow = Flow(n_dim=n_dim, bijection=RealNVP(n_dim=n_dim, context_shape=(context_dim,)))
+    flow = Flow(n_dim=n_dim, bijection=RealNVP(n_dim=n_dim, context_shape=(context_dim,)).to(device), device=device)
     x = torch.randn(n_data, n_dim)
     c = torch.randn(n_data, context_dim)
 
-    log_prob = flow.log_prob(x, context=c)
+    log_prob = flow.log_prob(x.to(device), context=c.to(device))
     assert log_prob.shape == (n_data,)
 
-    x_new = flow.sample(5, context=c)
+    x_new = flow.sample(5, context=c.to(device))
     assert x_new.shape == (5, n_data, n_dim)
 
     with pytest.raises(RuntimeError):
