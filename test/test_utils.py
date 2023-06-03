def9b4ea22b7a8cd9ccf5ab5c6ed6481c5e7ee21
David Nabergoj <davidnabergoj4@gmail.com>
2023-06-03 19:27:16 +0200

    Add batch shape test

---
+++ test/test_utils.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+import pytest
+import torch
+
+from src.utils import get_batch_shape
+
+
+@pytest.mark.parametrize('event_shape', [(1,), (2,), (3, 5), (5, 3), (1, 2, 3, 4)])
+@pytest.mark.parametrize('batch_shape', [(1,), (2,), (3, 5), (5, 3), (1, 2, 3, 4)])
+def test_batch_shape(event_shape: Tuple, batch_shape: Tuple):
+    torch.manual_seed(0)
+    event_shape = torch.Size(event_shape)
+    batch_shape = torch.Size(batch_shape)
+    x = torch.randn(*batch_shape, *event_shape)
+    assert get_batch_shape(x, event_shape) == batch_shape
