bdc04c00f007c25d47164c64785a760112403906
David Nabergoj <davidnabergoj4@gmail.com>
2023-06-07 12:28:54 +0200

    Add RQ spline test options

--- test/test_rq_spline.py
+++ test/test_rq_spline.py
@@ -1,15 +1,17 @@
+import pytest
 import torch
 
 from src.bijections.finite.autoregressive.transformers.spline import RationalQuadraticSpline
 
 
-def test_invertible():
+@pytest.mark.parametrize('n_data', [1, 2, 5, 100, 500])
+@pytest.mark.parametrize('n_dim', [1, 2, 5, 100, 500])
+@pytest.mark.parametrize('n_bins', [2, 4, 8, 16, 32])
+@pytest.mark.parametrize('scale', [1e-2, 1e-1, 1, 1e+1, 1e+2])
+def test_invertible(n_data, n_dim, n_bins, scale):
     torch.manual_seed(0)
-    n_bins = 8
-    n_data = 5
-    n_dim = 2
 
-    x = torch.randn(n_data, n_dim)
+    x = torch.randn(n_data, n_dim) * scale
     h = torch.randn(n_data, n_dim, 3 * n_bins - 1)
     spline = RationalQuadraticSpline(event_shape=torch.Size((n_dim,)), n_bins=n_bins)
     z, log_det_forward = spline.forward(x, h)
@@ -25,4 +27,4 @@ def test_invertible():
     assert torch.all(~torch.isnan(y))
     assert torch.all(~torch.isnan(log_det_inverse))
 
-    assert torch.allclose(log_det_forward + log_det_inverse, torch.zeros_like(log_det_forward), atol=1e-5)
+    assert torch.allclose(log_det_forward + log_det_inverse, torch.zeros_like(log_det_forward), atol=1e-3)
