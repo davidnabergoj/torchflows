a1b4173e01a1a767957ffb84cab0696362291c5e
David Nabergoj <davidnabergoj4@gmail.com>
2023-07-07 16:08:54 +0200

    Add householder, lower triangular bijection

--- test/test_bijection.py
+++ test/test_bijection.py
@@ -3,12 +3,13 @@ from typing import Tuple
 import pytest
 import torch
 
-from normalizing_flows.src.bijections.finite.linear import LU, Permutation, InverseLU
+from normalizing_flows.src.bijections.finite.linear import LU, Permutation, InverseLU, LowerTriangular, \
+    HouseholderOrthogonal
 
 
 @pytest.mark.parametrize('batch_shape', [(1,), (2,), (5,), (2, 4), (100,), (5, 1, 6, 7), (3, 13, 8)])
 @pytest.mark.parametrize('event_shape', [(2,), (3,), (2, 4), (100,), (50, 50)])
-@pytest.mark.parametrize('bijection_class', [LU, Permutation, InverseLU])
+@pytest.mark.parametrize('bijection_class', [LU, Permutation, InverseLU, LowerTriangular, HouseholderOrthogonal])
 def test_basic(batch_shape: Tuple, event_shape: Tuple, bijection_class):
     # Event shape cannot be too big, otherwise
     torch.manual_seed(0)
@@ -17,8 +18,9 @@ def test_basic(batch_shape: Tuple, event_shape: Tuple, bijection_class):
     z, log_det_forward = bij.forward(x)
     xr, log_det_inverse = bij.inverse(z)
 
-    assert torch.allclose(x, xr, atol=4e-4), f"{torch.max(torch.abs(x-xr)) = }"
-    assert torch.allclose(log_det_forward, -log_det_inverse, atol=1e-4), \
-        f"{torch.max(torch.abs(log_det_forward+log_det_inverse)) = }"
     assert x.shape == z.shape == xr.shape
     assert log_det_forward.shape == log_det_inverse.shape
+    assert torch.allclose(x, xr, atol=1e-3), f"{torch.max(torch.abs(x-xr)) = }"
+    assert torch.allclose(log_det_forward, -log_det_inverse, atol=1e-3), \
+        f"{torch.max(torch.abs(log_det_forward+log_det_inverse)) = }"
+
