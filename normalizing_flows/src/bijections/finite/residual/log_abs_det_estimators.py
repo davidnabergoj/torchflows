e4edde402d0138c9687d3aff3cf5872c7efd34ad
David Nabergoj <davidnabergoj4@gmail.com>
2023-07-12 23:41:04 +0200

    Use roulette estimator from ResFlow

--- normalizing_flows/src/bijections/finite/residual/log_abs_det_estimators.py
+++ normalizing_flows/src/bijections/finite/residual/log_abs_det_estimators.py
@@ -23,41 +23,136 @@ def log_det_hutchinson(g: callable, x: torch.Tensor, n_iterations: int = 8):
     return log_det
 
 
-def log_det_hutchinson_gradient(g: callable, x: torch.Tensor, n_iterations: int = 8):
-    raise NotImplementedError
-
-
 class LogDetHutchinson(torch.autograd.Function):
+    @staticmethod
     def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
         raise NotImplementedError
 
+    @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
         raise NotImplementedError
 
 
-def log_det_roulette(g: callable, x: torch.Tensor, dist: torch.distributions.Distribution = None):
-    if dist is None:
-        dist = Geometric(probs=torch.tensor(0.5), minimum=1)
-    n = dist.sample()
-    n_dim = x.shape[-1]
-    log_det = 0.0  # TODO batch the log det computation
-    v = torch.randn(n_dim, 1)
-    for k in range(1, n + 1):
-        vjp = torch.autograd.functional.vjp(func=g, inputs=v, create_graph=True) @ v
-        log_det += (-1.0) ** (k + 1) / k / dist.icdf(torch.tensor(k, dtype=torch.long)) * vjp
-    log_det /= n
-    return log_det
-
-
-def log_det_roulette_gradient(g: callable, x: torch.Tensor, dist: torch.distributions.Distribution = None):
-    if dist is None:
-        dist = Geometric(probs=torch.tensor(0.5), minimum=1)
-    raise NotImplementedError
+# class LogDetRoulette(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx: Any,
+#                 x: torch.Tensor,
+#                 g: callable,
+#                 p: float = 0.5,
+#                 training: bool = False) -> Any:
+#         # Compute log(abs{det[jac_x (x + g(x))]}) = log(abs{det[I + jac_x g(x)]})
+#         # https://github.com/rtqichen/residual-flows/blob/master/lib/layers/iresblock.py
+#         # This is an unbiased single sample approximation of the log determinant, i.e. we sample (n, v) once and
+#         # perform the estimation.
+#
+#         # n_dim = x.shape[-1]
+#         # log_det = 0.0  # TODO batch the log det computation
+#         # v = torch.randn(n_dim, 1)
+#         # vjp_list = []
+#         # for k in range(1, n + 1):
+#         #     vjp = torch.autograd.functional.vjp(func=g, inputs=v, create_graph=True)
+#         #     vjp_list.append(vjp)
+#         #     log_det += (-1.0) ** (k + 1) / k / dist.icdf(torch.tensor(k, dtype=torch.long)) * vjp @ v
+#         # log_det /= n
+#         # ctx.save_for_backward(*vjp_list)
+#         # return log_det
+#
+#         dist = Geometric(probs=torch.tensor(0.5), minimum=1)
+#         n = dist.sample()
+#         v = torch.randn_like(x)
+#
+#         # Note: torch.autograd.grad(y, x, z) = z * (\partial y / \partial x).
+#         # In this case, y is a vector, so the term in parentheses is the Jacobian matrix:
+#         #   torch.autograd.grad(y, x, z) = z * Jac
+#         # If z = v.T @ Jac^{k-1}, then we can do torch.autograd.grad(y, x, v.T @ Jac^{k-1}) = v.T @ Jac^k, which is
+#         # required for our computations.
+#         vjp = v  # Eventually holds v.T @ Jac^{n_iterations}
+#         neumann_result = v  # neumann_result @ v = log_det (we update neumann_result in the for loop)
+#         with torch.no_grad():
+#             for k in range(1, n + 1):
+#                 vjp = torch.autograd.grad(g, x, vjp, retain_graph=True)[0]
+#                 # P(N >= k) = 1 - P(N < k) = 1 - P(N <= k - 1) = 1 - cdf(k - 1)
+#                 p_k = 1 - dist.cdf(torch.tensor(k - 1, dtype=torch.long))
+#                 neumann_result += (-1) ** (k + 1) / (k * p_k) * vjp
+#         vjp_jac = torch.autograd.grad(g, x, neumann_result, create_graph=training)[0]
+#         logdetgrad = torch.sum(vjp_jac.view(x.shape[0], -1) * v.view(x.shape[0], -1), 1)
+#         return logdetgrad
+#
+#     @staticmethod
+#     def backward(ctx: Any, grad_output: torch.Tensor) -> Any:
+#         raise NotImplementedError
 
 
 class LogDetRoulette(torch.autograd.Function):
-    def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
-        raise NotImplementedError
+    @staticmethod
+    def neumann_logdet_estimator(g, x, vareps, training):
+        vjp = vareps
+        neumann_vjp = vareps
+        dist = Geometric(probs=torch.tensor(0.5), minimum=1)
+        n_power_series = int(dist.sample())
+        with torch.no_grad():
+            for k in range(1, n_power_series + 1):
+                vjp = torch.autograd.grad(g, x, vjp, retain_graph=True)[0]
+                # P(N >= k) = 1 - P(N < k) = 1 - P(N <= k - 1) = 1 - cdf(k - 1)
+                p_k = 1 - dist.cdf(torch.tensor(k - 1, dtype=torch.long))
+                neumann_vjp = neumann_vjp + (-1) ** k / (k * p_k) * vjp
+        vjp_jac = torch.autograd.grad(g, x, neumann_vjp, create_graph=training)[0]
+        logdetgrad = torch.sum(vjp_jac.view(x.shape[0], -1) * vareps.view(x.shape[0], -1), 1)
+        return logdetgrad
 
-    def backward(ctx: Any, *grad_outputs: Any) -> Any:
-        raise NotImplementedError
+    # https://github.com/rtqichen/residual-flows/blob/master/lib/layers/iresblock.py#L186
+    @staticmethod
+    def forward(ctx, gnet, x, vareps, training, *g_params):
+        ctx.training = training
+        with torch.enable_grad():
+            x = x.detach().requires_grad_(True)
+            g = gnet(x)
+            ctx.g = g
+            ctx.x = x
+            logdetgrad = LogDetRoulette.neumann_logdet_estimator(g, x, vareps, training)
+
+            if training:
+                grad_x, *grad_params = torch.autograd.grad(
+                    logdetgrad.sum(), (x,) + g_params, retain_graph=True, allow_unused=True
+                )
+                if grad_x is None:
+                    grad_x = torch.zeros_like(x)
+                ctx.save_for_backward(grad_x, *g_params, *grad_params)
+
+        return (
+            g.detach().requires_grad_(g.requires_grad),
+            logdetgrad.detach().requires_grad_(logdetgrad.requires_grad)
+        )
+
+    @staticmethod
+    def backward(ctx, grad_g, grad_logdetgrad):
+        training = ctx.training
+        if not training:
+            raise ValueError('Provide training=True if using backward.')
+
+        with torch.enable_grad():
+            grad_x, *params_and_grad = ctx.saved_tensors
+            g, x = ctx.g, ctx.x
+
+            # Precomputed gradients.
+            g_params = params_and_grad[:len(params_and_grad) // 2]
+            grad_params = params_and_grad[len(params_and_grad) // 2:]
+
+            dg_x, *dg_params = torch.autograd.grad(g, [x] + g_params, grad_g, allow_unused=True)
+
+        # Update based on gradient from logdetgrad.
+        dL = grad_logdetgrad[0].detach()
+        with torch.no_grad():
+            grad_x.mul_(dL)
+            grad_params = tuple([g.mul_(dL) if g is not None else None for g in grad_params])
+
+        # Update based on gradient from g.
+        with torch.no_grad():
+            grad_x.add_(dg_x)
+            grad_params = tuple([dg.add_(djac) if djac is not None else dg for dg, djac in zip(dg_params, grad_params)])
+
+        return (None, None, grad_x, None, None, None, None) + grad_params
+
+
+def log_det_roulette(g: callable, x: torch.Tensor):
+    return LogDetRoulette.apply(x, g)
