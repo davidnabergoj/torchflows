0ffcdc52965349c1ad84b0cc8e65b6c6cb184762
David Nabergoj <davidnabergoj4@gmail.com>
2023-07-19 18:23:32 +0200

    Implementing linear rational splines

--- normalizing_flows/src/bijections/finite/autoregressive/transformers/spline.py
+++ normalizing_flows/src/bijections/finite/autoregressive/transformers/spline.py
@@ -1,12 +1,12 @@
 import math
 from typing import Tuple, Union
 
-import matplotlib.pyplot as plt
+import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
 from normalizing_flows.src.bijections.finite.autoregressive.transformers.base import Transformer
-from normalizing_flows.src.utils import sum_except_batch
+from normalizing_flows.src.utils import sum_except_batch, get_batch_shape
 
 
 class MonotonicPiecewiseSpline(Transformer):
@@ -274,15 +274,61 @@ class RationalQuadraticSpline(Transformer):
         return self.rqs_caller(z, h, True)
 
 
-class LinearRationalSpline(Transformer):
-    def __init__(self, event_shape: Union[torch.Size, Tuple[int, ...]]):
+class LinearRationalSpline(MonotonicPiecewiseSpline):
+    def __init__(self, event_shape: Union[torch.Size, Tuple[int, ...]], n_bins: int = 8):
         super().__init__(event_shape)
+        self.n_knots = n_bins + 1
+        self.n_bins = n_bins
 
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        raise NotImplementedError
+        self.unconstrained_w0 = nn.Parameter(torch.tensor(0.0))
+        self.unconstrained_lambda = nn.Parameter(torch.zeros(size=(n_bins,)))
 
-    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        raise NotImplementedError
+    @property
+    def w0(self):
+        return torch.nn.functional.softplus(self.unconstrained_w0)
+
+    @property
+    def lmbd(self):
+        return torch.sigmoid(self.unconstrained_lambda)
+
+    def compute_parameters(self, h):
+        # We have knots (x, y, delta), but the derivatives at boundary knots are fixed to 1 (linear extrapolation)
+        assert h.shape[-1] == 3 * self.n_knots - 2
+
+        # Process inputs
+        # TODO put knot parsing into an inherited method, defined in MonotonicPiecewiseSpline
+        x = h[:self.n_knots]  # TODO cumsum etc
+        y = h[self.n_knots:2 * self.n_knots]  # TODO cumsum etc
+        delta = torch.nn.functional.softplus(h[2 * self.n_knots:])
+
+        batch_shape = h.shape[:-1]
+
+        # Compute w
+        w0 = (torch.zeros(size=batch_shape) + self.w0)[..., None]
+        delta_sqrt_ratios = torch.cat([w0, torch.sqrt(delta[..., :-1] / delta[..., 1:])], dim=-1)
+        w = torch.cumprod(delta_sqrt_ratios, dim=-1)
+
+        # Compute y(m)
+        lmbd = ...  # TODO repeat self.lmbd to batch shape
+        y_middle = torch.divide(
+            (1 - lmbd) * w[..., :-1] * y[..., :-1] + lmbd * w[..., 1:] * y[..., 1:],
+            (1 - lmbd) * w[..., :-1] + lmbd * w[..., 1:]
+        )
+
+        # Compute w(m)
+        s = (x[..., 1:] - x[..., -1:]) / (y[..., 1:] - y[..., -1:])
+        w_middle = s * torch.add(
+            lmbd * w[..., :-1] * delta[..., :-1],
+            (1 - lmbd) * w[..., 1:] * delta[..., 1:]
+        )
+
+        return x, y, delta, w, w_middle, y_middle
+
+    def forward_1d(self, x, h):
+        pass
+
+    def inverse_1d(self, y, h):
+        pass
 
 
 class BSpline(Transformer):
