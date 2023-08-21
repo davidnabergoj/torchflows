fbb05a64098c8b8d79d9330d31fa17eb1bc08de4
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-22 01:47:27 +0200

    Support arbitrary event shape in affine transformer

--- normalizing_flows/src/bijections/finite/autoregressive/transformers/affine.py
+++ normalizing_flows/src/bijections/finite/autoregressive/transformers/affine.py
@@ -16,14 +16,22 @@ class Affine(Transformer):
         alpha = self.scale_transform(h[..., 0]) + self.min_scale
         log_alpha = torch.log(alpha)
         beta = h[..., 1]
-        log_det = torch.sum(log_alpha, dim=-1)
+
+        n_event_dims = len(self.event_shape)
+        n_batch_dims = len(x.shape) - n_event_dims
+        event_dims = tuple(range(n_batch_dims, len(x.shape)))
+        log_det = torch.sum(log_alpha, dim=event_dims)
         return alpha * x + beta, log_det
 
     def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         alpha = self.scale_transform(h[..., 0]) + self.min_scale
         log_alpha = torch.log(alpha)
         beta = h[..., 1]
-        log_det = -torch.sum(log_alpha, dim=-1)
+
+        n_event_dims = len(self.event_shape)
+        n_batch_dims = len(z.shape) - n_event_dims
+        event_dims = tuple(range(n_batch_dims, len(z.shape)))
+        log_det = -torch.sum(log_alpha, dim=event_dims)
         return (z - beta) / alpha, log_det
 
 
