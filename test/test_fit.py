f9cd55b7fcbf0ae27ac04c128c4851926a94978b
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-25 01:03:16 +0200

    Minor changes to the fit test

--- test/test_fit.py
+++ test/test_fit.py
@@ -1,10 +1,10 @@
 import pytest
 import torch
-from normalizing_flows import NICE, RealNVP, MAF, ElementwiseAffine, ElementwiseShift, ElementwiseRQSpline, Flow
+from normalizing_flows import NICE, RealNVP, MAF, ElementwiseAffine, ElementwiseShift, ElementwiseRQSpline, Flow, CouplingRQNSF, MaskedAutoregressiveRQNSF
 
 
 @pytest.mark.parametrize('bijection_class', [
-    NICE, RealNVP, MAF, ElementwiseAffine, ElementwiseShift, ElementwiseRQSpline
+    ElementwiseAffine, ElementwiseShift, ElementwiseRQSpline, NICE, RealNVP, MAF, CouplingRQNSF, MaskedAutoregressiveRQNSF
 ])
 def test_standard_gaussian(bijection_class):
     torch.manual_seed(0)
@@ -14,9 +14,11 @@ def test_standard_gaussian(bijection_class):
     x = torch.randn(size=(n_data, n_dim))
 
     bijection = bijection_class(event_shape=(n_dim,))
-    flow = Flow(bijection)
 
-    flow.fit(x, n_epochs=100)
+    flow = Flow(bijection).cuda()
+    flow.fit(x, n_epochs=150)
+    flow = flow.cpu()
+
     x_flow = flow.sample(100_000)
 
     x_mean = torch.mean(x_flow, dim=0)
@@ -31,17 +33,13 @@ def test_diagonal_gaussian_elementwise_affine():
 
     n_data = 10_000
     n_dim = 3
-    sigma = torch.tensor([[1e-1, 1, 1e1]])
+    sigma = torch.tensor([[0.1, 1.0, 10.0]])
     x = torch.randn(size=(n_data, n_dim)) * sigma
 
     bijection = ElementwiseAffine(event_shape=(n_dim,))
     flow = Flow(bijection)
 
-    # print(bijection.conditioner_transform.theta)
-
-    flow.fit(x, n_epochs=500)
-
-    # print(bijection.conditioner_transform.theta)
+    flow.fit(x, n_epochs=250)
 
     x_flow = flow.sample(100_000)
     x_std = torch.std(x_flow, dim=0)
@@ -50,19 +48,19 @@ def test_diagonal_gaussian_elementwise_affine():
     assert relative_error < 0.1
 
 
-@pytest.mark.parametrize('bijection_class', [ElementwiseAffine, ElementwiseRQSpline, RealNVP, MAF])
+@pytest.mark.parametrize('bijection_class', [MaskedAutoregressiveRQNSF, ElementwiseRQSpline, ElementwiseAffine, RealNVP, MAF, CouplingRQNSF])
 def test_diagonal_gaussian_1(bijection_class):
     torch.manual_seed(0)
 
     n_data = 10_000
     n_dim = 3
-    sigma = torch.tensor([[1e-1, 1.0, 1e1]])
+    sigma = torch.tensor([[0.1, 1.0, 10.0]])
     x = torch.randn(size=(n_data, n_dim)) * sigma
 
     bijection = bijection_class(event_shape=(n_dim,))
     flow = Flow(bijection)
 
-    flow.fit(x, n_epochs=500)
+    flow.fit(x, n_epochs=250)
     x_flow = flow.sample(100_000)
     x_std = torch.std(x_flow, dim=0)
     relative_error = max((x_std - sigma.ravel()).abs() / sigma.ravel())
