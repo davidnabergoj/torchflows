629ad2f50944ad7f06f820a934056a81e72cb725
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-25 02:00:48 +0200

    Remove CUDA calls

--- test/test_fit.py
+++ test/test_fit.py
@@ -12,15 +12,10 @@ def test_standard_gaussian(bijection_class):
     n_data = 10_000
     n_dim = 10
     x = torch.randn(size=(n_data, n_dim))
-
     bijection = bijection_class(event_shape=(n_dim,))
-
-    flow = Flow(bijection).cuda()
+    flow = Flow(bijection)
     flow.fit(x, n_epochs=150)
-    flow = flow.cpu()
-
     x_flow = flow.sample(100_000)
-
     x_mean = torch.mean(x_flow, dim=0)
     x_var = torch.var(x_flow, dim=0)
 
@@ -35,12 +30,9 @@ def test_diagonal_gaussian_elementwise_affine():
     n_dim = 3
     sigma = torch.tensor([[0.1, 1.0, 10.0]])
     x = torch.randn(size=(n_data, n_dim)) * sigma
-
     bijection = ElementwiseAffine(event_shape=(n_dim,))
     flow = Flow(bijection)
-
     flow.fit(x, n_epochs=250)
-
     x_flow = flow.sample(100_000)
     x_std = torch.std(x_flow, dim=0)
     relative_error = max((x_std - sigma.ravel()).abs() / sigma.ravel())
@@ -56,10 +48,8 @@ def test_diagonal_gaussian_1(bijection_class):
     n_dim = 3
     sigma = torch.tensor([[0.1, 1.0, 10.0]])
     x = torch.randn(size=(n_data, n_dim)) * sigma
-
     bijection = bijection_class(event_shape=(n_dim,))
     flow = Flow(bijection)
-
     flow.fit(x, n_epochs=250)
     x_flow = flow.sample(100_000)
     x_std = torch.std(x_flow, dim=0)
