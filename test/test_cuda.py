7c62f1e84519218ddff1cb5691387274131e8e07
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-31 20:06:11 +0200

    Add CUDA test

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -3,6 +3,30 @@ from normalizing_flows.bijections import RealNVP
 from normalizing_flows import Flow
 
 
+def test_real_nvp_log_prob_data_on_cpu():
+    torch.manual_seed(0)
+
+    batch_shape = (3, 5)
+    event_shape = (7, 11)
+
+    x_train = torch.randn(*batch_shape, *event_shape)
+
+    flow = Flow(RealNVP(event_shape)).cuda()
+    flow.log_prob(x_train)
+
+
+def test_real_nvp_log_prob_data_on_gpu():
+    torch.manual_seed(0)
+
+    batch_shape = (3, 5)
+    event_shape = (7, 11)
+
+    x_train = torch.randn(*batch_shape, *event_shape)
+
+    flow = Flow(RealNVP(event_shape)).cuda()
+    flow.log_prob(x_train.cuda())
+
+
 def test_real_nvp_fit_data_on_cpu():
     torch.manual_seed(0)
 
