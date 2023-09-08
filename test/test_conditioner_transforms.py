65c077150d84ee9423c507fd9910e38516c3754b
David Nabergoj <davidnabergoj4@gmail.com>
2023-09-08 16:37:27 +0200

    Update conditioner transforms test

--- test/test_conditioner_transforms.py
+++ test/test_conditioner_transforms.py
@@ -4,6 +4,7 @@ import torch
 from normalizing_flows.bijections.finite.autoregressive.conditioner_transforms import (
     MADE, FeedForward, QuasiMADE, LinearMADE, ResidualFeedForward, Constant, Linear
 )
+from test.constants import __test_constants
 
 
 @pytest.mark.parametrize('transform_class', [
@@ -14,29 +15,17 @@ from normalizing_flows.bijections.finite.autoregressive.conditioner_transforms i
     ResidualFeedForward,
     Linear
 ])
-def test_nontrivial_shape(transform_class):
-    batch_shape = (3,)
-    event_shape = (2, 4)
-    n_parameters = 5
-
+@pytest.mark.parametrize('batch_shape', __test_constants['batch_shape'])
+@pytest.mark.parametrize('input_event_shape', __test_constants['input_event_shape'])
+@pytest.mark.parametrize('output_event_shape', __test_constants['output_event_shape'])
+@pytest.mark.parametrize('n_predicted_parameters', __test_constants['n_predicted_parameters'])
+def test_shape(transform_class, batch_shape, input_event_shape, output_event_shape, n_predicted_parameters):
     torch.manual_seed(0)
-    x = torch.randn(size=(*batch_shape, *event_shape))
-
-    transform = transform_class(input_event_shape=event_shape, output_event_shape=event_shape, n_predicted_parameters=n_parameters)
+    x = torch.randn(size=(*batch_shape, *input_event_shape))
+    transform = transform_class(
+        input_event_shape=input_event_shape,
+        output_event_shape=output_event_shape,
+        n_predicted_parameters=n_predicted_parameters
+    )
     out = transform(x)
-
-    assert out.shape == (*batch_shape, *event_shape, n_parameters)
-
-
-def test_constant_transform_nontrivial_shape():
-    batch_shape = (3,)
-    event_shape = (2, 4)
-    n_parameters = 5
-
-    torch.manual_seed(0)
-    x = torch.randn(size=(*batch_shape, *event_shape))
-
-    transform = Constant(event_shape, n_parameters=n_parameters)
-    out = transform(x)
-
-    assert out.shape == (*batch_shape, *event_shape, n_parameters)
+    assert out.shape == (*batch_shape, *output_event_shape, n_predicted_parameters)
