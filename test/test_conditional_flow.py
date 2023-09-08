57f50ae29cf486afafe1a0a066093b161992034c
David Nabergoj <davidnabergoj4@gmail.com>
2023-09-08 10:00:03 +0200

    Remove CUDA in conditional flow tests

--- test/test_conditional_flow.py
+++ test/test_conditional_flow.py
@@ -7,20 +7,19 @@ from normalizing_flows import RealNVP, Flow
 
 @pytest.mark.parametrize('n_dim', [2, 10, 100])
 @pytest.mark.parametrize('context_dim', [1, 2, 10, 100])
-@pytest.mark.parametrize('device', ['cpu', 'cuda'])
-def test_context(n_dim, context_dim, device: str):
+def test_context(n_dim, context_dim):
     torch.manual_seed(0)
-    device = torch.device(device)
 
     n_data = 100
-    flow = Flow(RealNVP(n_dim, context_shape=(context_dim,))).to(device)
+    event_shape = torch.Size((n_dim,))
+    flow = Flow(RealNVP(event_shape, context_shape=(context_dim,)))
     x = torch.randn(n_data, n_dim)
     c = torch.randn(n_data, context_dim)
 
-    log_prob = flow.log_prob(x.to(device), context=c.to(device))
+    log_prob = flow.log_prob(x, context=c)
     assert log_prob.shape == (n_data,)
 
-    x_new = flow.sample(5, context=c.to(device))
+    x_new = flow.sample(5, context=c)
     assert x_new.shape == (5, n_data, n_dim)
 
     with pytest.raises(RuntimeError):
@@ -32,7 +31,8 @@ def test_no_context(n_dim):
     torch.manual_seed(0)
 
     n_data = 100
-    flow = Flow(RealNVP(n_dim))
+    event_shape = torch.Size((n_dim,))
+    flow = Flow(RealNVP(event_shape))
     x = torch.randn(n_data, n_dim)
 
     log_prob = flow.log_prob(x)
@@ -51,19 +51,18 @@ def test_no_context(n_dim):
 @pytest.mark.parametrize('context_dim', [1, 2, 3, 768])
 @pytest.mark.parametrize('n_dim', [2, 10, 100])
 @pytest.mark.parametrize('n_data', [1, 2, 5, 10])
-@pytest.mark.parametrize('device', ['cpu', 'cuda'])
-def test_training(n_dim: int, context_dim: int, n_data: int, device: str):
+def test_training(n_dim: int, context_dim: int, n_data: int):
     torch.manual_seed(0)
-    device = torch.device(device)
-    flow = Flow(RealNVP(n_dim, context_shape=(context_dim,))).to(device)
+    event_shape = torch.Size((n_dim,))
+    flow = Flow(RealNVP(event_shape, context_shape=(context_dim,)))
     optimizer = optim.AdamW(flow.parameters())
     x = torch.randn(n_data, n_dim)
     c = torch.randn(n_data, context_dim)
     for _ in range(5):
         optimizer.zero_grad()
-        loss = -flow.log_prob(x.to(device), context=c.to(device)).mean()
+        loss = -flow.log_prob(x, context=c).mean()
         loss.backward()
         optimizer.step()
 
-    samples = flow.sample(23, context=c.to(device))
+    samples = flow.sample(23, context=c)
     assert samples.shape == (23, n_data, n_dim)
