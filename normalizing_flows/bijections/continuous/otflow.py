b6a44bc0df39d9022bd4116a12b19bdfbfa220a5
David Nabergoj <davidnabergoj4@gmail.com>
2023-09-15 14:16:01 +0200

    Implementing OT-Flow

--- normalizing_flows/bijections/continuous/otflow.py
+++ normalizing_flows/bijections/continuous/otflow.py
@@ -30,8 +30,17 @@ class OTResNet(nn.Module):
     def sigma(x):
         return torch.log(torch.exp(x) + torch.exp(-x))
 
+    @staticmethod
+    def sigma_prime(x):
+        return torch.tanh(x)
+
+    @staticmethod
+    def sigma_prime_prime(x):
+        # Square of the hyperbolic secant
+        return 4 / torch.square(torch.exp(x) + torch.exp(-x))
+
     def compute_u0(self, s):
-        u0 = torch.nn.functional.linear(s, self.K0, self.b0)
+        u0 = self.sigma(torch.nn.functional.linear(s, self.K0, self.b0))
         return u0
 
     def forward(self, s):
@@ -43,20 +52,47 @@ class OTResNet(nn.Module):
         u1 = u0 + self.step_size * self.sigma(torch.nn.functional.linear(u0, self.K1, self.b1))
         return u1
 
+    def compute_z1(self, s, w, u0: torch.Tensor = None):
+        if u0 is None:
+            u0 = self.compute_u0(s)
+        linear_in = torch.matmul(torch.diag(self.sigma_prime(torch.nn.functional.linear(u0, self.K1, self.b1))), w)
+        z1 = w + self.step_size * torch.nn.functional.linear(linear_in, self.K1.T)
+        return z1
+
     def jvp(self, s, w):
         """
         Compute grad_ResNet_wrt_s * w. The first term is the jacobian matrix.
         """
-        u0 = self.compute_u0(s)
-        linear_in = torch.matmul(torch.diag(torch.tanh(torch.nn.functional.linear(u0, self.K1, self.b1))), w)
-        z1 = w + self.step_size * torch.nn.functional.linear(linear_in, self.K1.T)
-
-        linear_in_2 = torch.matmul(torch.diag(torch.tanh(torch.nn.functional.linear(s, self.K0, self.b0))), z1)
+        z1 = self.compute_z1(s, w)
+        linear_in_2 = torch.matmul(torch.diag(self.sigma_prime(torch.nn.functional.linear(s, self.K0, self.b0))), z1)
         z0 = torch.nn.functional.linear(linear_in_2, self.K0.T)
         return z0
 
+    def hessian_trace(self,
+                      s: torch.Tensor = None,
+                      w: torch.Tensor = None,
+                      u0: torch.Tensor = None,
+                      z1: torch.Tensor = None):
+        if u0 is None:
+            u0 = self.compute_u0(s)
+        if z1 is None:
+            z1 = self.compute_z1(s, w, u0=u0)
+        # Compute the first term in Equation 14
+        t0 = torch.matmul(
+            (self.sigma_prime_prime(torch.nn.functional.linear(s, self.K0, self.b0)) * z1).T,
+            self.K0[:, :-1] ** 2 @ torch.ones(size=(self.K0.shape[0] - 1,))
+        )
 
-class OTNetwork(DifferentialEquationNeuralNetwork):
+        K1J = self.K0.T @ self.sigma_prime(torch.nn.functional.linear(s, self.K0, self.b0))
+        t1 = torch.matmul(
+            (self.sigma_prime_prime(torch.nn.functional.linear(u0, self.K1, self.b1)) * z1).T,
+            K1J ** 2 @ torch.ones(size=(self.K0.shape[0] - 1,))
+        )
+
+        return t0 + self.step_size * t1
+
+
+class OTPotential(DifferentialEquationNeuralNetwork):
     # TODO introduce a new superclass which OTNetwork and DifferentialEquationNeuralNetwork both inherit.
     def __init__(self, event_size: int, hidden_size: int, **kwargs):
         # hidden_size = m
@@ -69,8 +105,25 @@ class OTNetwork(DifferentialEquationNeuralNetwork):
 
     def forward(self, t, x):
         s = ...  # TODO concatentate x and t. x goes first, t is at the end.
-        grad = self.resnet.jvp(s, self.w) + self.A.T @ self.A @ s + self.b
-        return -grad
+        return -self.gradient(s)
+
+    def gradient(self, s):
+        # Equation 12
+        return self.resnet.jvp(s, self.w) + self.A.T @ self.A @ s + self.b
+
+    def hessian_trace(self):
+        # Equation 14
+
+        K0E = self.resnet.K0[:, :-1]
+
+        t0 = self.resnet.sigma_prime_prime(self.resnet)
+        tr_first_term = t0 + self.resnet.step_size * t1
+
+        # E.T @ A ... remove last row (assuming E has d of d+1 standard basis vectors)
+        # A @ E ... remove last column (assuming E has d of d+1 standard basis vectors)
+        tr_second_term = torch.trace((self.A.T @ self.A)[:-1, :-1])
+
+        return tr_first_term + tr_second_term
 
 
 class OTFlow(ContinuousBijection):
