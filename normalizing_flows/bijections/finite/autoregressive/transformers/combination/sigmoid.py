42071b17eebbf96ac86e6bfe18b1cdc72baf5648
David Nabergoj <davidnabergoj4@gmail.com>
2023-09-19 17:13:03 +0200

    Implementing DDSF

--- normalizing_flows/bijections/finite/autoregressive/transformers/combination/sigmoid.py
+++ normalizing_flows/bijections/finite/autoregressive/transformers/combination/sigmoid.py
@@ -1,12 +1,19 @@
 import math
 from typing import Tuple, Union
 import torch
+import torch.nn as nn
 from normalizing_flows.bijections.finite.autoregressive.transformers.base import Transformer
 from normalizing_flows.bijections.finite.autoregressive.transformers.combination.base import Combination
 from normalizing_flows.bijections.finite.autoregressive.transformers.combination.sigmoid_util import log_softmax, \
-    log_sigmoid
+    log_sigmoid, log_dot
 from normalizing_flows.bijections.numerical_inversion import bisection_no_gradient
 from normalizing_flows.utils import sum_except_batch
+
+
+# As defined in the NAF paper
+
+def inverse_sigmoid(p):
+    return torch.log(p / (1 - p))
 
 
 class Sigmoid(Transformer):
@@ -39,7 +46,7 @@ class Sigmoid(Transformer):
 
     def extract_parameters(self, h: torch.Tensor):
         """
-        h.shape = (*b, *e, self.n_parameters)
+        h.shape = (batch_size, self.n_parameters)
         """
         da = h[:, :self.hidden_dim]
         db = h[:, self.hidden_dim:self.hidden_dim * 2]
@@ -54,34 +61,26 @@ class Sigmoid(Transformer):
 
     def forward_1d(self, x, h):
         """
-        x.shape = (n,)
-        h.shape = (n, n_hidden * 3)
+        x.shape = (batch_size,)
+        h.shape = (batch_size, hidden_size * 3)
 
         Within the function:
-        a.shape = (n, n_hidden)
-        b.shape = (n, n_hidden)
-        w.shape = (n, n_hidden)
+        a.shape = (batch_size, hidden_size)
+        b.shape = (batch_size, hidden_size)
+        w.shape = (batch_size, hidden_size)
         """
         a, b, w, log_w = self.extract_parameters(h)
-        c = torch.sigmoid(a * x[:, None] + b)  # (*b, *e, n_hidden)
-        d = torch.einsum('...i,...i->...', w, c)  # Softmax weighing -> (*b, *e)
-        x = torch.log(d / (1 - d))  # Inverse sigmoid (*b, *e)
+        c = torch.sigmoid(a * x[:, None] + b)  # (batch_size, n_hidden)
+        d = torch.einsum('...i,...i->...', w, c)  # Softmax weighing -> (batch_size,)
+        x = inverse_sigmoid(d)  # Inverse sigmoid ... (batch_size,)
 
-        log_t1 = (torch.log(d) - torch.log(1 - d))[:, None]  # (n, h)
-        log_t2 = log_w  # (n, h)
-        log_t3 = (log_sigmoid(c) + log_sigmoid(-c))  # (n, h)
-        log_t4 = torch.log(a)  # (n, h)
+        log_t1 = (torch.log(d) - torch.log(1 - d))[:, None]  # (batch_size, hidden_size)
+        log_t2 = log_w  # (batch_size, hidden_size)
+        log_t3 = (log_sigmoid(c) + log_sigmoid(-c))  # (batch_size, hidden_size)
+        log_t4 = torch.log(a)  # (batch_size, hidden_size)
         log_det = torch.sum(log_t1 + log_t2 + log_t3 + log_t4, dim=1)
 
         z = x
-        return z, log_det
-
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        x_flat = x.view(-1)
-        h_flat = h.view(-1, h.shape[-1])
-        z_flat, log_det_flat = self.forward_1d(x_flat, h_flat)
-        z = z_flat.view_as(x)
-        log_det = sum_except_batch(log_det_flat.view_as(x), self.event_shape)
         return z, log_det
 
     def inverse_1d(self, z, h):
@@ -90,6 +89,14 @@ class Sigmoid(Transformer):
 
         x, log_det = bisection_no_gradient(f, z)
         return x, log_det
+
+    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        x_flat = x.view(-1)
+        h_flat = h.view(-1, h.shape[-1])
+        z_flat, log_det_flat = self.forward_1d(x_flat, h_flat)
+        z = z_flat.view_as(x)
+        log_det = sum_except_batch(log_det_flat.view_as(x), self.event_shape)
+        return z, log_det
 
     def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         z_flat = z.view(-1)
@@ -100,15 +107,156 @@ class Sigmoid(Transformer):
         return x, log_det
 
 
-class DenseSigmoid(Transformer):
-    def __init__(self, event_shape: Union[torch.Size, Tuple[int, ...]], **kwargs):
-        super().__init__(event_shape)
+class DenseSigmoidInnerTransform(nn.Module):
+    def __init__(self, input_size, output_size, min_scale: float = 1e-3):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.min_scale = min_scale
+        self.const = 1000
+        self.default_u_a = math.log(math.e - 1 - self.min_scale)
+
+    @property
+    def n_parameters(self):
+        return self.output_size ** 2 + 2 * self.output_size + self.output_size * self.input_size
+
+    def extract_parameters(self, h: torch.Tensor):
+        """
+        h.shape = (batch_size, self.n_parameters)
+        """
+        batch_size = len(h)
+
+        da = h[:, :self.output_size]
+        db = h[:, self.output_size:self.output_size * 2]
+        dw = h[:, self.output_size * 2:self.output_size * 2 + self.output_size ** 2]
+        du = h[:, self.output_size * 2 + self.output_size ** 2:]
+
+        du = du.view(batch_size, self.output_size, self.input_size)
+        dw = dw.view(batch_size, self.output_size, self.output_size)
+
+        u_pre = 0.0 + du / self.const
+        u = torch.softmax(u_pre, dim=-1)
+        log_u = log_softmax(u_pre, dim=-1)
+
+        a = torch.nn.functional.softplus(self.default_u_a + da / self.const) + self.min_scale
+        b = db / self.const
+        w_pre = 0.0 + dw / self.const
+        w = torch.softmax(w_pre, dim=-1)
+        log_w = log_softmax(w_pre, dim=-1)
+        return a, b, w, log_w, u, log_u
+
+    def forward_1d(self, x, h):
+        # Compute y = inv_sig(w @ sig(a * u @ x + b))
+        # h.shape = (batch_size, n_parameters)
+
+        # Within the function:
+        # x.shape = (batch_size, input_size)
+        # a.shape = (batch_size, output_size)
+        # b.shape = (batch_size, output_size)
+        # w.shape = (batch_size, output_size, output_size)
+        # log_w.shape = (batch_size, output_size, output_size)
+        # u.shape = (batch_size, output_size, input_size)
+        # log_u.shape = (batch_size, output_size, input_size)
+
+        # Return
+        # y.shape = (batch_size, output_size)
+        # log_det.shape = (batch_size,)
+
+        a, b, w, log_w, u, log_u = self.extract_parameters(h)
+
+        ux = torch.einsum('boi,bi->bo', u, x)  # (batch_size, output_size)
+        c = torch.sigmoid(a * ux + b)  # (batch_size, output_size)
+        d = torch.einsum('bij,bj->bi', w, c)  # Softmax weighing -> (batch_size, output_size)
+        x = inverse_sigmoid(d)  # Inverse sigmoid (batch_size, output_size)
+
+        log_t1 = (torch.log(d) - torch.log(1 - d))[:, :, None]  # (batch_size, output_size, 1)
+        log_t2 = log_w  # (batch_size, output_size, output_size)
+        log_t3 = (log_sigmoid(c) + log_sigmoid(-c))[:, None, :]  # (batch_size, 1, output_size)
+        log_t4 = torch.log(a)[:, None, :]  # (batch_size, 1, output_size)
+
+        m1 = (log_t1 + log_t2 + log_t3 + log_t4)[:, :, :, None]  # (batch_size, output_size, output_size, 1)
+        m2 = log_u[:, None, :, :]  # (batch_size, 1, output_size, input_size)
+        log_det = torch.sum(log_dot(m1, m2), dim=(1, 2, 3))
+
+        z = x
+        return z, log_det
+
+    def inverse_1d(self, z, h):
+        def f(inputs):
+            return self.forward_1d(inputs, h)
+
+        x, log_det = bisection_no_gradient(f, z)
+        return x, log_det
 
     def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        pass
+        x_flat = x.view(-1, self.input_size)
+        h_flat = h.view(-1, self.n_parameters)
+        z_flat, log_det_flat = self.forward_1d(x_flat, h_flat)
+        z = z_flat.view_as(x)
+        log_det = sum_except_batch(log_det_flat.view_as(x), self.event_shape)
+        return z, log_det
 
     def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        pass
+        z_flat = z.view(-1, self.output_size)
+        h_flat = h.view(-1, self.n_parameters)
+        x_flat, log_det_flat = self.inverse_1d(z_flat, h_flat)
+        x = x_flat.view_as(z)
+        log_det = sum_except_batch(log_det_flat.view_as(z), self.event_shape)
+        return x, log_det
+
+
+class DenseSigmoid(Transformer):
+    def __init__(self,
+                 event_shape: Union[torch.Size, Tuple[int, ...]],
+                 n_dense_layers: int = 1,
+                 hidden_size: int = 30):
+        super().__init__(event_shape)
+        self.n_dense_layers = n_dense_layers
+        layers = [
+            DenseSigmoidInnerTransform(self.n_dim, hidden_size),
+            *[DenseSigmoidInnerTransform(hidden_size, hidden_size) for _ in range(n_dense_layers)],
+            DenseSigmoidInnerTransform(hidden_size, self.n_dim),
+        ]
+        self.layers = nn.ModuleList(layers)
+
+    @property
+    def n_parameters(self) -> int:
+        return sum([layer.n_parameters for layer in self.layers])
+
+    @property
+    def default_parameters(self) -> torch.Tensor:
+        return torch.zeros(size=(self.n_parameters,))  # TODO set up parametrization with deltas so this holds
+
+    def split_parameters(self, h):
+        # split parameters h into parameters for several layers
+        # h.shape == (*batch_shape, *event_shape, n_parameters)
+        split_parameters = torch.split(h, split_size_or_sections=[layer.n_parameters for layer in self.layers])
+        return split_parameters
+
+    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        h_split = self.split_parameters(h)
+        log_det = None
+        for i in range(len(self.layers)):
+            x, log_det_inc = self.layers[i].forward(x, h_split[i])
+            if log_det is None:
+                log_det = log_det_inc
+            else:
+                log_det += log_det_inc
+        z = x
+        return z, log_det
+
+    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        h_split = self.split_parameters(h)
+        log_det = None
+        for i in range(len(self.layers) - 1, -1, -1):
+            z, log_det_inc = self.layers[i].inverse(z, h_split[i])
+            if log_det is None:
+                log_det = log_det_inc
+            else:
+                log_det += log_det_inc
+        x = z
+        return x, log_det
 
 
 class DeepSigmoid(Combination):
