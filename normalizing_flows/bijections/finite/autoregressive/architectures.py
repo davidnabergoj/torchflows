f4805e947049594319ccd48938fb9a9f9bb15268
David Nabergoj <davidnabergoj4@gmail.com>
2023-11-13 14:56:58 -0800

    Replace elementwise affine with elementwise shift in CouplingLRS flows for improved numerical stability

--- normalizing_flows/bijections/finite/autoregressive/architectures.py
+++ normalizing_flows/bijections/finite/autoregressive/architectures.py
@@ -11,7 +11,7 @@ from normalizing_flows.bijections.finite.autoregressive.layers import (
     ElementwiseAffine,
     UMNNMaskedAutoregressive,
     LRSCoupling,
-    LRSForwardMaskedAutoregressive
+    LRSForwardMaskedAutoregressive, ElementwiseShift
 )
 from normalizing_flows.bijections.base import BijectiveComposition
 from normalizing_flows.bijections.finite.linear import ReversePermutation
@@ -127,13 +127,13 @@ class CouplingLRS(BijectiveComposition):
     def __init__(self, event_shape, n_layers: int = 2, **kwargs):
         if isinstance(event_shape, int):
             event_shape = (event_shape,)
-        bijections = [ElementwiseAffine(event_shape=event_shape)]
+        bijections = [ElementwiseShift(event_shape=event_shape)]
         for _ in range(n_layers):
             bijections.extend([
                 ReversePermutation(event_shape=event_shape),
                 LRSCoupling(event_shape=event_shape)
             ])
-        bijections.append(ElementwiseAffine(event_shape=event_shape))
+        bijections.append(ElementwiseShift(event_shape=event_shape))
         super().__init__(event_shape, bijections, **kwargs)
 
 
