48a14f561a009d56aee53ae02e2fb1ea983feedb
David Nabergoj <davidnabergoj4@gmail.com>
2023-11-16 10:11:48 -0800

    Add intermediate parameter reshape method to transformers, modify affine transformer parameter count

--- normalizing_flows/bijections/finite/autoregressive/transformers/affine.py
+++ normalizing_flows/bijections/finite/autoregressive/transformers/affine.py
@@ -24,15 +24,12 @@ class Affine(Transformer):
 
     @property
     def n_parameters(self) -> int:
-        return 2
+        return 2 * self.n_dim
 
-    @property
-    def default_parameters(self) -> torch.Tensor:
-        default_u_alpha = torch.zeros(size=(1,))
-        default_u_beta = torch.zeros(size=(1,))
-        return torch.cat([default_u_alpha, default_u_beta], dim=0)
+    def unflatten_conditioner_parameters(self, h: torch.Tensor):
+        return torch.unflatten(h, dim=-1, sizes=(*self.event_shape, 2))
 
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward_base(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         u_alpha = h[..., 0]
         alpha = torch.exp(self.identity_unconstrained_alpha + u_alpha / self.const) + self.m
         log_alpha = torch.log(alpha)
@@ -43,7 +40,7 @@ class Affine(Transformer):
         log_det = sum_except_batch(log_alpha, self.event_shape)
         return alpha * x + beta, log_det
 
-    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def inverse_base(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         u_alpha = h[..., 0]
         alpha = torch.exp(self.identity_unconstrained_alpha + u_alpha / self.const) + self.m
         log_alpha = torch.log(alpha)
@@ -55,80 +52,24 @@ class Affine(Transformer):
         return (z - beta) / alpha, log_det
 
 
-class Affine2(Transformer):
-    """
-    Affine transformer with near-identity initialization.
-
-    Computes z = alpha * x + beta, where alpha > 0 and -inf < beta < inf.
-    Alpha and beta have the same shape as x, i.e. the computation is performed elementwise.
-
-    In this implementation, we compute alpha and beta such that the initial map is near identity.
-    We also use a minimum permitted scale m, 0 < m <= alpha, for numerical stability
-    This means setting alpha = 1 + d(u_alpha) where -1 + m < d(u_alpha) < inf.
-
-    We can verify that the following construction for function d is suitable:
-    * g(u) = u / c + log(-log(m)); c>=1, 0<m<1  # c > 0 is enough, but c >= 1 desirably reduces input magnitude
-    * f(u) = exp(g(u)) + log(m)
-    * d(u) = exp(f(u)) - 1
-
-    A change in u implies a change in log(log(d)), we may need a bigger step size when optimizing parameters of
-    overarching bijections that use this transformer.
-    """
-
-    def __init__(self, event_shape: torch.Size, min_scale: float = 1e-3, **kwargs):
-        super().__init__(event_shape=event_shape)
-        assert 0 < min_scale < 1
-        self.m = min_scale
-        self.log_m = math.log(self.m)
-        self.log_neg_log_m = math.log(-self.log_m)
-        self.c = 100.0
-
-    def compute_scale_and_shift(self, h):
-        u_alpha = h[..., 0]
-        g_alpha = u_alpha / self.c + self.log_neg_log_m
-        f_alpha = torch.exp(g_alpha) + self.log_m
-        d_alpha = torch.exp(f_alpha) - 1
-        # d_alpha = self.m ** (1 - torch.exp(u_alpha / self.c)) - 1  # Rewritten
-        # d_alpha = self.m * self.m ** (-torch.exp(u_alpha / self.c)) - 1  # Rewritten again
-
-        alpha = 1 + d_alpha
-
-        u_beta = h[..., 1]
-        d_beta = u_beta / self.c
-        beta = 0 + d_beta
-
-        return alpha, beta
-
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        alpha, beta = self.compute_scale_and_shift(h)
-        log_det = sum_except_batch(torch.log(alpha), self.event_shape)
-        return alpha * x + beta, log_det
-
-    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        alpha, beta = self.compute_scale_and_shift(h)
-        log_det = -sum_except_batch(torch.log(alpha), self.event_shape)
-        return (z - beta) / alpha, log_det
-
-
 class Shift(Transformer):
     def __init__(self, event_shape: torch.Size):
         super().__init__(event_shape=event_shape)
 
     @property
     def n_parameters(self) -> int:
-        return 1
+        return 1 * self.n_dim
 
-    @property
-    def default_parameters(self) -> torch.Tensor:
-        return torch.zeros(size=(1,))
+    def unflatten_conditioner_parameters(self, h: torch.Tensor):
+        return torch.unflatten(h, dim=-1, sizes=(*self.event_shape, 1))
 
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward_base(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         beta = h[..., 0]
         batch_shape = get_batch_shape(x, self.event_shape)
         log_det = torch.zeros(batch_shape, device=x.device)
         return x + beta, log_det
 
-    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def inverse_base(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         beta = h[..., 0]
         batch_shape = get_batch_shape(z, self.event_shape)
         log_det = torch.zeros(batch_shape, device=z.device)
@@ -151,12 +92,12 @@ class Scale(Transformer):
 
     @property
     def n_parameters(self) -> int:
-        return 1
+        return 1 * self.n_dim
 
-    def default_parameters(self) -> torch.Tensor:
-        return torch.zeros(size=(1,))
+    def unflatten_conditioner_parameters(self, h: torch.Tensor):
+        return torch.unflatten(h, dim=-1, sizes=(*self.event_shape, 1))
 
-    def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward_base(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         u_alpha = h[..., 0]
         alpha = torch.exp(self.u_alpha_1 + u_alpha / self.const) + self.m
         log_alpha = torch.log(alpha)
@@ -164,7 +105,7 @@ class Scale(Transformer):
         log_det = sum_except_batch(log_alpha, self.event_shape)
         return alpha * x, log_det
 
-    def inverse(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def inverse_base(self, z: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         u_alpha = h[..., 0]
         alpha = torch.exp(self.u_alpha_1 + u_alpha / self.const) + self.m
         log_alpha = torch.log(alpha)
--- normalizing_flows/bijections/finite/autoregressive/transformers/base.py
+++ normalizing_flows/bijections/finite/autoregressive/transformers/base.py
@@ -6,27 +6,89 @@ from normalizing_flows.bijections.base import Bijection
 
 
 class Transformer(Bijection):
+    """
+    Base transformer class.
+
+    A transformer receives as input a batch of tensors x with x.shape = (*batch_shape, *event_shape) and a
+     corresponding batch of parameter tensors h with h.shape = (*batch_shape, self.n_parameters). It outputs transformed
+     tensors z with z.shape = (*batch_shape, *event_shape).
+    Given parameters h, a transformer is bijective. Transformers apply bijections of the same kind to a batch of inputs,
+     but use different parameters for each input.
+
+    When implementing new transformers, consider the self.unflatten_conditioner_parameters method, which is used to
+     optionally reshape transformer parameters into a suitable shape.
+    """
+
     def __init__(self, event_shape: Union[torch.Size, Tuple[int, ...]]):
         super().__init__(event_shape=event_shape)
 
+    def forward_base(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Apply forward bijection to a batch of inputs x, parameterizing each bijection with the corresponding parameter
+         tensor in h.
+
+        :param x: input tensor with x.shape = (*batch_shape, *event_shape).
+        :param h: parameter tensor with h.shape = (*batch_shape, *parameter_shape).
+        """
+        raise NotImplementedError
+
     def forward(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Apply forward bijection to a batch of inputs x, parameterizing each bijection with the corresponding parameter
+         tensor in h.
+
+        :param x: input tensor with x.shape = (*batch_shape, *event_shape).
+        :param h: parameter tensor with h.shape = (*batch_shape, self.n_parameters).
+        """
+        return self.forward_base(x, self.unflatten_conditioner_parameters(h))
+
+    def inverse_base(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Apply inverse bijection to a batch of inputs x, parameterizing each bijection with the corresponding parameter
+         tensor in h.
+
+        :param x: input tensor with x.shape = (*batch_shape, *event_shape).
+        :param h: parameter tensor with h.shape = (*batch_shape, *parameter_shape).
+        """
         raise NotImplementedError
 
     def inverse(self, x: torch.Tensor, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        raise NotImplementedError
+        """
+        Apply inverse bijection to a batch of inputs x, parameterizing each bijection with the corresponding parameter
+         tensor in h.
+
+        :param x: input tensor with x.shape = (*batch_shape, *event_shape).
+        :param h: parameter tensor with h.shape = (*batch_shape, self.n_parameters).
+        """
+        return self.inverse_base(x, self.unflatten_conditioner_parameters(h))
+
+    def unflatten_conditioner_parameters(self, h: torch.Tensor):
+        """
+        Reshapes parameter tensors as predicted by the conditioner.
+        The new shape facilitates operations in the transformer and facilitates transformer operations.
+        If this method is not overwritten, the default parameter tensor shape is kept.
+
+        :param h: batch of parameter tensors for each input event with shape (*batch_shape, self.n_parameters).
+        :return: batch of parameter tensors for each input event with shape (*batch_shape, *new_shape)
+        """
+        return h
 
     @property
     def n_parameters(self) -> int:
         """
-        Number of parameters that parametrize this transformer. Example: rational quadratic splines require 3*b-1 where
-        b is the number of bins. An affine transformation requires 2 (typically corresponding to the unconstrained scale
-        and shift).
+        Number of parameters that parametrize this transformer.
+
+        Examples:
+            * Rational quadratic splines require (3 * b - 1) * d where b is the number of bins and d is the
+              dimensionality, equal to the product of all dimensions of the transformer input tensor.
+            * An affine transformation requires 2 * d (typically corresponding to the unconstrained scale and shift).
         """
         raise NotImplementedError
 
     @property
     def default_parameters(self) -> torch.Tensor:
         """
-        Set of parameters which ensures an identity transformation.
+        Set of parameters which yields a close-to-identity transformation.
+        These are set to 0 by default.
         """
-        raise NotImplementedError
+        return torch.zeros(size=(self.n_parameters,))
