2560814ed228cbd75778c8dee356e44346e8f34a
David Nabergoj <davidnabergoj4@gmail.com>
2023-12-27 22:19:28 +0100

    Use different residual blocks in Invertible ResNet and ResFlow

--- normalizing_flows/bijections/finite/residual/architectures.py
+++ normalizing_flows/bijections/finite/residual/architectures.py
@@ -14,15 +14,19 @@ from normalizing_flows.bijections.finite.residual.sylvester import Sylvester
 
 class InvertibleResNet(ResidualComposition):
     def __init__(self, event_shape, context_shape=None, n_layers: int = 16, **kwargs):
-        block = InvertibleResNetBlock(event_shape=event_shape, context_shape=context_shape, **kwargs)
-        blocks = [block for _ in range(n_layers)]  # The same block
+        blocks = [
+            InvertibleResNetBlock(event_shape=event_shape, context_shape=context_shape, **kwargs)
+            for _ in range(n_layers)
+        ]
         super().__init__(blocks)
 
 
 class ResFlow(ResidualComposition):
     def __init__(self, event_shape, context_shape=None, n_layers: int = 16, **kwargs):
-        block = ResFlowBlock(event_shape=event_shape, context_shape=context_shape, **kwargs)
-        blocks = [block for _ in range(n_layers)]  # The same block
+        blocks = [
+            ResFlowBlock(event_shape=event_shape, context_shape=context_shape, **kwargs)
+            for _ in range(n_layers)
+        ]
         super().__init__(blocks)
 
 
