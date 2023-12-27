abb32dce10cc79f2d405bd81911074aed7a3278f
David Nabergoj <davidnabergoj4@gmail.com>
2023-12-27 22:17:04 +0100

    Fix log determinant in residual bijection, interleave layers with elementwise affine maps

--- normalizing_flows/bijections/finite/residual/base.py
+++ normalizing_flows/bijections/finite/residual/base.py
@@ -2,6 +2,7 @@ from typing import Union, Tuple, List
 
 import torch
 
+from normalizing_flows.bijections.finite.autoregressive.layers import ElementwiseAffine
 from normalizing_flows.bijections.base import Bijection, BijectiveComposition
 from normalizing_flows.utils import get_batch_shape, unflatten_event, flatten_event
 
@@ -32,7 +33,7 @@ class ResidualBijection(Bijection):
         else:
             x_flat = flatten_event(x, self.event_shape).clone()
             x_flat.requires_grad_(True)
-            log_det = self.log_det(x_flat, training=self.training)
+            log_det = -self.log_det(x_flat, training=self.training)
 
         return z, log_det
 
@@ -59,8 +60,15 @@ class ResidualBijection(Bijection):
 class ResidualComposition(BijectiveComposition):
     def __init__(self, blocks: List[ResidualBijection]):
         assert len(blocks) > 0
+        event_shape = blocks[0].event_shape
+
+        updated_layers = [ElementwiseAffine(event_shape)]
+        for i in range(len(blocks)):
+            updated_layers.append(blocks[i])
+            updated_layers.append(ElementwiseAffine(event_shape))
+
         super().__init__(
-            event_shape=blocks[0].event_shape,
-            layers=blocks,
-            context_shape=blocks[0].context_shape
+            event_shape=updated_layers[0].event_shape,
+            layers=updated_layers,
+            context_shape=updated_layers[0].context_shape
         )
