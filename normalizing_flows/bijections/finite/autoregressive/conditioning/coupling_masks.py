84ad0623ca42d0e1e93ce118b2df38daf812a571
David Nabergoj <davidnabergoj4@gmail.com>
2024-06-06 13:17:44 +0200

    Add checkerboard mask and test

--- normalizing_flows/bijections/finite/autoregressive/conditioning/coupling_masks.py
+++ normalizing_flows/bijections/finite/autoregressive/conditioning/coupling_masks.py
@@ -73,6 +73,29 @@ class HalfSplit(Coupling):
         super().__init__(event_shape, mask=torch.less(torch.arange(event_size).view(*event_shape), event_size // 2))
 
 
+class Checkerboard(Coupling):
+    """
+    Checkerboard coupling for image data.
+    """
+
+    def __init__(self, event_shape, resolution: int = 2):
+        """
+        :param event_shape: image shape with the form (n_channels, width, height). Note: width and height must be equal
+        and a power of two.
+        :param resolution: resolution of the checkerboard along one axis - the number of squares. Must be a power of two
+        and smaller than image width.
+        """
+        n_channels, width, _ = event_shape
+        assert width % resolution == 0
+        square_side_length = width // resolution
+        assert resolution % 2 == 0
+        half_resolution = resolution // 2
+        a = torch.tensor([[1, 0] * half_resolution, [0, 1] * half_resolution] * half_resolution)
+        mask = torch.kron(a, torch.ones((square_side_length, square_side_length)))
+        mask = mask.bool()
+        super().__init__(event_shape, mask)
+
+
 def make_coupling(event_shape, edge_list: List[Tuple[int, int]] = None):
     if edge_list is None:
         return HalfSplit(event_shape)
---
+++ test/test_checkerboard_coupling.py
@@ -0,0 +1,46 @@
+from normalizing_flows.bijections.finite.autoregressive.conditioning.coupling_masks import Checkerboard
+import torch
+
+
+def test_checkerboard_small():
+    torch.manual_seed(0)
+    image_shape = (3, 4, 4)
+    coupling = Checkerboard(image_shape, resolution=2)
+    assert torch.allclose(
+        coupling.source_mask,
+        torch.tensor([
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 1, 1],
+            [0, 0, 1, 1],
+        ], dtype=torch.bool)[None].repeat(3, 1, 1)
+    )
+    assert torch.allclose(coupling.target_mask, ~coupling.source_mask)
+
+
+def test_checkerboard_medium():
+    torch.manual_seed(0)
+    image_shape = (3, 16, 16)
+    coupling = Checkerboard(image_shape, resolution=4)
+    assert torch.allclose(
+        coupling.source_mask,
+        torch.tensor([
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+        ], dtype=torch.bool)[None].repeat(3, 1, 1)
+    )
+    assert torch.allclose(coupling.target_mask, ~coupling.source_mask)
