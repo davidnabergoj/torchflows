71a92ed6c284ab879a6ed0be5ea0512b066d1bea
David Nabergoj <davidnabergoj4@gmail.com>
2024-09-01 00:15:58 +0200

    Add docstrings for class constructors

--- torchflows/bijections/finite/autoregressive/architectures.py
+++ torchflows/bijections/finite/autoregressive/architectures.py
@@ -59,7 +59,12 @@ class NICE(AutoregressiveArchitecture):
     Reference: Dinh et al. "NICE: Non-linear Independent Components Estimation" (2015); https://arxiv.org/abs/1410.8516.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to ShiftCoupling.
+        """
         super().__init__(event_shape, base_bijection=ShiftCoupling, **kwargs)
 
 
@@ -69,7 +74,12 @@ class RealNVP(AutoregressiveArchitecture):
     Reference: Dinh et al. "Density estimation using Real NVP" (2017); https://arxiv.org/abs/1605.08803.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to AffineCoupling.
+        """
         super().__init__(event_shape, base_bijection=AffineCoupling, **kwargs)
 
 
@@ -79,7 +89,12 @@ class InverseRealNVP(AutoregressiveArchitecture):
     Reference: Dinh et al. "Density estimation using Real NVP" (2017); https://arxiv.org/abs/1605.08803.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to InverseAffineCoupling.
+        """
         super().__init__(event_shape, base_bijection=InverseAffineCoupling, **kwargs)
 
 
@@ -89,7 +104,12 @@ class MAF(AutoregressiveArchitecture):
     Reference: Papamakarios et al. "Masked Autoregressive Flow for Density Estimation" (2018); https://arxiv.org/abs/1705.07057.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to AffineForwardMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=AffineForwardMaskedAutoregressive, **kwargs)
 
 
@@ -99,7 +119,12 @@ class IAF(AutoregressiveArchitecture):
     Reference: Kingma et al. "Improving Variational Inference with Inverse Autoregressive Flow" (2017); https://arxiv.org/abs/1606.04934.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to AffineInverseMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=AffineInverseMaskedAutoregressive, **kwargs)
 
 
@@ -109,7 +134,12 @@ class CouplingRQNSF(AutoregressiveArchitecture):
     Reference: Durkan et al. "Neural Spline Flows" (2019); https://arxiv.org/abs/1906.04032.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to RQSCoupling.
+        """
         super().__init__(event_shape, base_bijection=RQSCoupling, **kwargs)
 
 
@@ -119,7 +149,12 @@ class MaskedAutoregressiveRQNSF(AutoregressiveArchitecture):
     Reference: Durkan et al. "Neural Spline Flows" (2019); https://arxiv.org/abs/1906.04032.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to RQSForwardMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=RQSForwardMaskedAutoregressive, **kwargs)
 
 
@@ -129,7 +164,12 @@ class CouplingLRS(AutoregressiveArchitecture):
     Reference: Dolatabadi et al. "Invertible Generative Modeling using Linear Rational Splines" (2020); https://arxiv.org/abs/2001.05168.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to LRSCoupling.
+        """
         super().__init__(event_shape, base_bijection=LRSCoupling, **kwargs)
 
 
@@ -139,7 +179,12 @@ class MaskedAutoregressiveLRS(AutoregressiveArchitecture):
     Reference: Dolatabadi et al. "Invertible Generative Modeling using Linear Rational Splines" (2020); https://arxiv.org/abs/2001.05168.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to LRSForwardMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=LRSForwardMaskedAutoregressive, **kwargs)
 
 
@@ -149,7 +194,12 @@ class InverseAutoregressiveRQNSF(AutoregressiveArchitecture):
     Reference: Durkan et al. "Neural Spline Flows" (2019); https://arxiv.org/abs/1906.04032.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to RQSInverseMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=RQSInverseMaskedAutoregressive, **kwargs)
 
 
@@ -159,7 +209,12 @@ class InverseAutoregressiveLRS(AutoregressiveArchitecture):
     Reference: Dolatabadi et al. "Invertible Generative Modeling using Linear Rational Splines" (2020); https://arxiv.org/abs/2001.05168.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to LRSInverseMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=LRSInverseMaskedAutoregressive, **kwargs)
 
 
@@ -169,7 +224,12 @@ class CouplingDeepSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to DeepSigmoidalCoupling.
+        """
         super().__init__(event_shape, base_bijection=DeepSigmoidalCoupling, **kwargs)
 
 
@@ -179,7 +239,12 @@ class InverseAutoregressiveDeepSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to DeepSigmoidalInverseMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=DeepSigmoidalInverseMaskedAutoregressive, **kwargs)
 
 
@@ -189,7 +254,12 @@ class MaskedAutoregressiveDeepSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to DeepSigmoidalForwardMaskedAutoregressive.
+        """
         super().__init__(event_shape, base_bijection=DeepSigmoidalForwardMaskedAutoregressive, **kwargs)
 
 
@@ -199,7 +269,17 @@ class CouplingDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DenseSigmoidalCoupling.
+        """
         super().__init__(
             event_shape,
             base_bijection=DenseSigmoidalCoupling,
@@ -214,7 +294,17 @@ class InverseAutoregressiveDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DenseSigmoidalInverseMaskedAutoregressive.
+        """
         super().__init__(
             event_shape,
             base_bijection=DenseSigmoidalInverseMaskedAutoregressive,
@@ -229,7 +319,17 @@ class MaskedAutoregressiveDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DenseSigmoidalForwardMaskedAutoregressive.
+        """
         super().__init__(
             event_shape,
             base_bijection=DenseSigmoidalForwardMaskedAutoregressive,
@@ -244,7 +344,17 @@ class CouplingDeepDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DeepDenseSigmoidalCoupling.
+        """
         super().__init__(
             event_shape,
             base_bijection=DeepDenseSigmoidalCoupling,
@@ -259,7 +369,17 @@ class InverseAutoregressiveDeepDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DeepDenseSigmoidalInverseMaskedAutoregressive.
+        """
         super().__init__(
             event_shape,
             base_bijection=DeepDenseSigmoidalInverseMaskedAutoregressive,
@@ -274,7 +394,17 @@ class MaskedAutoregressiveDeepDenseSF(AutoregressiveArchitecture):
     Reference: Huang et al. "Neural Autoregressive Flows" (2018); https://arxiv.org/abs/1804.00779.
     """
 
-    def __init__(self, event_shape, percentage_global_parameters: float = 0.8, **kwargs):
+    def __init__(self,
+                 event_shape: Union[Tuple[int, ...], torch.Size, int],
+                 percentage_global_parameters: float = 0.8,
+                 **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param percentage_global_parameters: percentage of transformer inputs to be learned globally instead of being
+         predicted from the conditioner neural network.
+        :param kwargs: keyword arguments to DeepDenseSigmoidalForwardMaskedAutoregressive.
+        """
         super().__init__(
             event_shape,
             base_bijection=DeepDenseSigmoidalForwardMaskedAutoregressive,
@@ -289,7 +419,12 @@ class UMNNMAF(AutoregressiveArchitecture):
     Reference: Wehenkel and Louppe "Unconstrained Monotonic Neural Networks" (2021); https://arxiv.org/abs/1908.05164.
     """
 
-    def __init__(self, event_shape, **kwargs):
+    def __init__(self, event_shape: Union[Tuple[int, ...], torch.Size, int], **kwargs):
+        """
+
+        :param event_shape: shape of the event tensor.
+        :param kwargs: keyword arguments to UMNNMaskedAutoregressive.
+        """
         super().__init__(
             event_shape,
             base_bijection=UMNNMaskedAutoregressive,
