25179e8767d29df5fe30a099d1921b747d7ab38d
David Nabergoj <davidnabergoj4@gmail.com>
2024-10-19 20:45:28 +0200

    Set version to v1.1.0

--- torchflows/_version.py
+++ torchflows/_version.py
@@ -1,12 +1,1 @@
-from pathlib import Path
-
-try:
-    import toml
-
-    pyproject_toml_file = Path(__file__).parents[1] / "pyproject.toml"
-    data = toml.load(pyproject_toml_file)
-    __version__ = data["project"]["version"]
-except ImportError:
-    import importlib.metadata
-
-    __version__ = importlib.metadata.version("torchflows")
+__version__ = "v1.1.0"
