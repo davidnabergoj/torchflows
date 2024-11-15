31b1a08e174b9f5f48323d803b47c933aa9261ce
David Nabergoj <davidnabergoj4@gmail.com>
2024-11-15 12:16:31 +0100

    Fix RNODE deepcopy (hack)

--- test/test_deepcopy.py
+++ test/test_deepcopy.py
@@ -16,5 +16,11 @@ def test_post_variational_fit():
     b = RNODE(event_shape=(10,))
     f = Flow(b)
     f.variational_fit(lambda x: torch.sum(-x ** 2), n_epochs=2)
-    b.eval()
+    deepcopy(b)
+
+def test_post_fit():
+    torch.manual_seed(0)
+    b = RNODE(event_shape=(10,))
+    f = Flow(b)
+    f.fit(x_train=torch.randn(3, *b.event_shape), n_epochs=2)
     deepcopy(b)
--- torchflows/flows.py
+++ torchflows/flows.py
@@ -6,6 +6,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 from tqdm import tqdm
+
+from torchflows.bijections.continuous.rnode import RNODE
 from torchflows.bijections.base import Bijection
 from torchflows.utils import flatten_event, unflatten_event, create_data_loader
 from torchflows.base_distributions.gaussian import DiagonalGaussian
@@ -260,6 +262,10 @@ class BaseFlow(nn.Module):
         if x_val is not None and keep_best_weights:
             self.load_state_dict(best_weights)
 
+        # hacky error handling (Jacobian regularization is a non-leaf node within RNODE's autograd graph)
+        if isinstance(self.bijection, RNODE):
+            self.bijection.f.stored_reg = None
+
         self.eval()
 
     def variational_fit(self,
@@ -272,7 +278,7 @@ class BaseFlow(nn.Module):
                         keep_best_weights: bool = True,
                         show_progress: bool = False,
                         check_for_divergences: bool = False,
-                        time_limit_seconds:Union[float, int] = None):
+                        time_limit_seconds: Union[float, int] = None):
         """Train the normalizing flow to fit a target log probability.
 
         Stochastic variational inference lets us train a distribution using the unnormalized target log density instead of a fixed dataset.
@@ -366,6 +372,10 @@ class BaseFlow(nn.Module):
             self.load_state_dict(initial_weights)
         elif keep_best_weights:
             self.load_state_dict(best_weights)
+
+        # hacky error handling (Jacobian regularization is a non-leaf node within RNODE's autograd graph)
+        if isinstance(self.bijection, RNODE):
+            self.bijection.f.stored_reg = None
 
         self.eval()
 
