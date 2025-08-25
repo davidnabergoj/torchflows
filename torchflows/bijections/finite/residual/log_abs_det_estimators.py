8cab3d89a48eb1e35d125d7753bd5068a99f3cdd
David Nabergoj <davidnabergoj4@gmail.com>
2025-08-25 18:55:55 +0200

    Roulette and power series log determinant improvements

--- torchflows/bijections/finite/residual/log_abs_det_estimators.py
+++ torchflows/bijections/finite/residual/log_abs_det_estimators.py
@@ -46,35 +46,58 @@ def power_series_log_abs_det_estimator(g: callable,
     assert n_iterations >= 2
 
     w = noise  # (batch_size, event_size, n_hutchinson_samples)
-    log_abs_det_jac_f = torch.zeros(size=(batch_size,)).to(x)
+    log_abs_det_jac_f = torch.zeros(
+        size=(batch_size,), 
+        device=x.device, 
+        dtype=x.dtype
+    )
     g_value = None
+
     for k in range(1, n_iterations + 1):
-        # Compute VJP, reshape appropriately for hutchinson averaging
-        gs_r, ws_r = torch.autograd.functional.vjp(
-            g,
-            x[..., None].repeat(*([1] * (len(event_shape) + 1)), n_hutchinson_samples).view(
-                batch_size * n_hutchinson_samples,
-                *event_shape
-            ),
-            w.view(batch_size * n_hutchinson_samples, *event_shape),
-            create_graph=training
+        # Reshape tensors for hutchinson averaging
+        x_rep = x[..., None].repeat(
+            *([1] * (len(event_shape) + 1)), 
+            n_hutchinson_samples
+        ).view(
+            batch_size * n_hutchinson_samples, 
+            *event_shape
         )
+        w_flat = w.view(batch_size * n_hutchinson_samples, *event_shape)
+
+        # Compute VJP
+        if training:
+            gs_r, ws_r = torch.autograd.functional.vjp(
+                g, x_rep, w_flat, create_graph=True
+            )
+        else:
+            with torch.no_grad():
+                gs_r, ws_r = torch.autograd.functional.vjp(
+                    g, x_rep, w_flat, create_graph=False
+                )
 
         if g_value is None:
-            g_value = gs_r.view(batch_size, *event_shape, n_hutchinson_samples)[..., 0]
+            g_value = gs_r.view(
+                batch_size, 
+                *event_shape,
+                n_hutchinson_samples
+            )[..., 0]
 
         w = ws_r.view(batch_size, *event_shape, n_hutchinson_samples)
 
         # sum over event dims, average over hutchinson dim
-        log_abs_det_jac_f += (-1) ** (k + 1) / k * torch.sum(w * noise, dim=event_dims).mean(dim=1)
+        log_abs_det_jac_f += (
+            ((-1) ** (k + 1)) / k
+            * torch.sum(w * noise, dim=event_dims).mean(dim=1)
+        )
         assert log_abs_det_jac_f.shape == (batch_size,)
+
     return g_value, log_abs_det_jac_f
 
 
-def roulette_log_abs_det_estimator(g: callable,
-                                   x: torch.Tensor,
-                                   noise: torch.Tensor,
-                                   training: bool,
+def roulette_log_abs_det_estimator(g, 
+                                   x, 
+                                   noise, 
+                                   training: bool, 
                                    p: float = 0.5):
     """
     Estimate log[abs(det(grad(f)))](x) with a roulette approach, where f(x) = x + g(x); Lip(g) < 1.
@@ -85,21 +108,59 @@ def roulette_log_abs_det_estimator(g: callable,
     :param training: is the computation being performed for a model being trained.
     :return:
     """
-    # f(x) = x + g(x)
-    event_dims = tuple(range(1, len(x.shape)))
+    event_dims = tuple(range(1, x.dim()))
     w = noise
     neumann_vjp = noise
-    dist = Geometric(probs=torch.tensor(p), minimum=1)
-    n_power_series = int(dist.sample())
-    with torch.no_grad():
+
+    dist = Geometric(
+        probs=torch.tensor(
+            p, 
+            device=x.device, 
+            dtype=x.dtype
+        ), 
+        minimum=1
+    )
+    n_power_series = int(dist.sample().item())
+
+    # Build or skip graphs depending on training
+    def vjp(v, cg): return torch.autograd.functional.vjp(
+        g, 
+        x, 
+        v, 
+        create_graph=cg
+    )[1]
+
+    if training:
         for k in range(1, n_power_series + 1):
-            # w = torch.autograd.grad(g_value, x, w, retain_graph=True)[0]
-            g_value, w = torch.autograd.functional.vjp(g, x, w)
-            # P(N >= k) = 1 - P(N < k) = 1 - P(N <= k - 1) = 1 - cdf(k - 1)
-            p_k = 1 - dist.cdf(torch.tensor(k - 1, dtype=torch.long))
-            neumann_vjp = neumann_vjp + (-1) ** k / (k * p_k) * w
-    g_value, vjp_jac = torch.autograd.functional.vjp(g, x, neumann_vjp, create_graph=training)
-    # vjp_jac = torch.autograd.grad(g_value, x, neumann_vjp, create_graph=training)[0]
+            w = vjp(w, True)
+            p_k = 1 - dist.cdf(
+                torch.tensor(
+                    k - 1,
+                    device=x.device, 
+                    dtype=torch.long
+                )
+            )
+            neumann_vjp = neumann_vjp + ((-1) ** k) / (k * p_k) * w
+    else:
+        with torch.no_grad():
+            for k in range(1, n_power_series + 1):
+                w = vjp(w, False)
+                p_k = 1 - dist.cdf(
+                    torch.tensor(
+                        k - 1,
+                        device=x.device, 
+                        dtype=torch.long
+                    )
+                )
+                neumann_vjp = neumann_vjp + ((-1) ** k) / (k * p_k) * w
+
+    g_value, vjp_jac = torch.autograd.functional.vjp(
+        g, 
+        x, 
+        neumann_vjp, 
+        create_graph=training
+    )
+
     log_abs_det_jac_f = torch.sum(vjp_jac * noise, dim=event_dims)
     return g_value, log_abs_det_jac_f
 
@@ -110,67 +171,74 @@ class LogDeterminantEstimator(torch.autograd.Function):
     Autodiff support permits this function to be used in a computation graph.
     """
 
-    # https://github.com/rtqichen/residual-flows/blob/master/resflows/layers/iresblock.py#L249
     @staticmethod
     def forward(ctx,
                 estimator_function: callable,
-                g: nn.Module,
+                g: torch.Tensor,
                 x: torch.Tensor,
                 noise: torch.Tensor,
                 training: bool,
-                *g_params):
+                *g_params: torch.Tensor):
+        # Save what we need to recompute in backward
         ctx.training = training
-        with torch.enable_grad():
-            ctx.x = x
+        ctx.estimator_function = estimator_function
+        ctx.g = g
+        ctx.save_for_backward(x, noise, *g_params)
+
+        # Forward values without gradient pre-computation
+        with torch.set_grad_enabled(training):
             g_value, log_det_f = estimator_function(g, x, noise, training)
-            ctx.g_value = g_value
 
-            if training:
-                # If a model is being trained,
-                # compute the gradient of the log determinant in the forward pass and store it for later.
-                # The gradient is computed w.r.t. x (first output) and w.r.t. the parameters of g (following outputs).
-                grad_x, *grad_params = torch.autograd.grad(
-                    log_det_f.sum(), (x,) + g_params, retain_graph=True, allow_unused=True
-                )
-                if grad_x is None:
-                    grad_x = torch.zeros_like(x)
-                ctx.save_for_backward(grad_x, *g_params, *grad_params)
-
-        # Return g(x) and log(abs(det(grad(f))))(x)
         return (
             g_value.detach().requires_grad_(g_value.requires_grad),
-            log_det_f.detach().requires_grad_(log_det_f.requires_grad)
+            log_det_f.detach().requires_grad_(log_det_f.requires_grad),
         )
 
     @staticmethod
-    def backward(ctx, grad_g, grad_logdetgrad):
-        training = ctx.training
-        if not training:
-            raise ValueError('Provide training=True if using backward.')
+    def backward(ctx, grad_g, grad_logdet):
+        if not ctx.training:
+            raise ValueError("Provide training=True if using backward.")
 
+        estimator_function = ctx.estimator_function
+        g = ctx.g
+        tensors = ctx.saved_tensors
+        x, noise = tensors[0], tensors[1]
+        g_params = list(tensors[2:])
+
+        # Gradients from g(x, params)
         with torch.enable_grad():
-            grad_x, *params_and_grad = ctx.saved_tensors
-            g_value, x = ctx.g_value, ctx.x
+            x_ = x.detach().requires_grad_(True)
+            for p in g_params:
+                if p is not None:
+                    p.requires_grad_(True)
+            g_value = g(x_)
+            dg_x, *dg_params = torch.autograd.grad(
+                g_value, [x_] + g_params,
+                grad_g, 
+                allow_unused=True
+            )
 
-            # Precomputed gradients
-            g_params = params_and_grad[:len(params_and_grad) // 2]
-            grad_params = params_and_grad[len(params_and_grad) // 2:]
+        # Gradients from the log determinant
+        with torch.enable_grad():
+            x2 = x.detach().requires_grad_(True)
+            _, log_det_f = estimator_function(g, x2, noise, training=True)
+            gl_x, *gl_params = torch.autograd.grad(
+                log_det_f, (x2,) + tuple(g_params),
+                grad_outputs=grad_logdet, allow_unused=True
+            )
 
-            dg_x, *dg_params = torch.autograd.grad(g_value, [x] + g_params, grad_g, allow_unused=True,
-                                                   retain_graph=training)
+        # Combine
+        def add(a, b):
+            if a is None:
+                return b
+            if b is None:
+                return a
+            return a + b
 
-        # Update based on gradient from log determinant.
-        dL = grad_logdetgrad[0].detach()
-        with torch.no_grad():
-            grad_x.mul_(dL)
-            grad_params = tuple([g.mul_(dL) if g is not None else None for g in grad_params])
+        grad_x = add(dg_x, gl_x)
+        grad_params = [add(a, b) for a, b in zip(dg_params, gl_params)]
 
-        # Update based on gradient from g.
-        with torch.no_grad():
-            grad_x.add_(dg_x)
-            grad_params = tuple([dg.add_(djac) if djac is not None else dg for dg, djac in zip(dg_params, grad_params)])
-
-        return (None, None, grad_x, None, None) + grad_params
+        return (None, None, grad_x, None, None) + tuple(grad_params)
 
 
 def log_det_roulette(event_shape: Union[torch.Size, Tuple[int, ...]],
@@ -182,7 +250,11 @@ def log_det_roulette(event_shape: Union[torch.Size, Tuple[int, ...]],
     x = flatten_batch(x, batch_shape)
     noise = torch.randn_like(x).to(x)
     return LogDeterminantEstimator.apply(
-        lambda *args, **kwargs: roulette_log_abs_det_estimator(*args, **kwargs, p=p),
+        lambda *args, **kwargs: roulette_log_abs_det_estimator(
+            *args, 
+            **kwargs, 
+            p=p
+        ),
         g,
         x,
         noise,
@@ -201,7 +273,11 @@ def log_det_power_series(event_shape: Union[torch.Size, Tuple[int, ...]],
     x = flatten_batch(x, batch_shape)
     noise = torch.randn(size=(*x.shape, n_hutchinson_samples)).to(x)
     return LogDeterminantEstimator.apply(
-        lambda *args, **kwargs: power_series_log_abs_det_estimator(*args, **kwargs, n_iterations=n_iterations),
+        lambda *args, **kwargs: power_series_log_abs_det_estimator(
+            *args, 
+            **kwargs, 
+            n_iterations=n_iterations
+        ),
         g,
         x,
         noise,
